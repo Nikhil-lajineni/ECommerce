0fc23e7d7dfa3d7a1618fd2e9523375fe43459d2
nikhil <nikhillajineni2@gmail.com>
2024-01-27 20:20:16 +0530

    "Joined Table and Single Table inheritance"

--- src/main/java/com/example/springboot/Models/BaseModel.java
+++ src/main/java/com/example/springboot/Models/BaseModel.java
@@ -1,6 +1,8 @@
 package com.example.springboot.Models;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import java.util.Date;
 
 @Data
 @MappedSuperclass
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) ->to create table for parent table also
 public class BaseModel {
     @Id
     private Long id;
